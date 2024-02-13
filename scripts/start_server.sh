bf555a7534a5b495f50d9d16ac821990891b3696
Tamas Nyulas <tamas.nyulas@spryker.com>
2024-02-13 21:53:17 +0100

    Introduced appspec.yml

---
+++ scripts/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Start the server
+echo "Starting snake-wars node server..."
+
+node /var/www/snake-wars/server/server.js
+
---
+++ scripts/stop_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Stop the Node.js server
+echo "Stopping snake-wars node server..."
+killall node
